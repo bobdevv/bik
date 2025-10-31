4f5f37cac3d47de57e859775ce12ce6e9b13c8e6
bobdevv <99577249+bobdevv@users.noreply.github.com>
2025-10-31 12:09:29 +0330

    Delete src/gui directory

--- src/gui/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "gui/MainWindow.h"
-#include <FL/Fl.H>
-
-int main(int argc, char* argv[]) {
-    bik::MainWindow window(800, 600, "Bik - Backup Manager");
-    window.show(argc, argv);
-    return Fl::run();
-}
